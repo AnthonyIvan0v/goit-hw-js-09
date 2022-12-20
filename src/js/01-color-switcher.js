d2c580ba983233ca1c3e10b7a72a2bc38413d29c
AnthonyIvan0v <cat.sledgehammer@gmail.com>
2022-12-20 15:58:06 +0200

    add code first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const refs = {
+    buttonStart: document.querySelector('button[data-start]'),
+    buttonStop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+};
+const DELAY = 1000;
+let timer = null;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+refs.buttonStop.setAttribute('disabled', true);
+  
+refs.buttonStart.addEventListener('click', onStartClick);
+refs.buttonStop.addEventListener('click', onStopClick);
+
+function onStopClick() {
+    refs.buttonStart.removeAttribute('disabled');
+    clearInterval(timer);
+    refs.buttonStop.setAttribute('disabled', true);
+  };
+ 
+function onStartClick() {
+    refs.buttonStart.setAttribute('disabled', true);
+    refs.buttonStop.removeAttribute('disabled');
+    timer = setInterval(() => {
+      refs.body.style.backgroundColor = getRandomHexColor();
+    }, DELAY)
+  };
+  
+
 
